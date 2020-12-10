c5438a3597c7d1654bdb9eedf811108bca7f4f87
JE-Chen <zenmailman@gmail.com>
2020-12-11 01:26:15 +0800

    edit UnitTest
    edit UnitTest

--- src/test/java/Moudle/ClientEndPoint_JavaxTest.java
+++ src/test/java/Moudle/ClientEndPoint_JavaxTest.java
@@ -9,19 +9,31 @@ class ClientEndPoint_JavaxTest {
 
     @Test
     void testConnect() {
-        try {
-            final ClientEndPoint_Javax clientEndPoint = new ClientEndPoint_Javax(new URI("ws://localhost:5050/websocket"));
-            clientEndPoint.addMessageHandler(new ClientEndPoint_Javax.MessageHandler() {
-                public void handleMessage(String message) {
-                    System.out.println(message);
+        Runnable testRun =new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ClientEndPoint_Javax clientEndPoint = new ClientEndPoint_Javax(new URI("ws://localhost:5050/websocket"));
+                    clientEndPoint.addMessageHandler(new ClientEndPoint_Javax.MessageHandler() {
+                        public void handleMessage(String message) {
+                            System.out.println(message);
+                        }
+                    });
+                    clientEndPoint.sendMessage("Hello Server");
+                } catch (
+                        URISyntaxException ex) {
+                    System.err.println("URISyntaxException exception: " + ex.getMessage());
                 }
-            });
-            clientEndPoint.sendMessage("Hello Server");
+            }
+        };
+        Thread testThread = new Thread(testRun);
+        testThread.setDaemon(true);
+        testThread.start();
+        try {
             Thread.sleep(5000);
-            System.exit(1);
-        } catch (URISyntaxException | InterruptedException ex) {
-            System.err.println("URISyntaxException exception: " + ex.getMessage());
+            System.exit(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-
 }
