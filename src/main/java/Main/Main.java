8940a7c20d91369c2339f8de55295d15a0a1e848
JE-Chen <zenmailman@gmail.com>
2020-12-28 10:44:50 +0800

    refactor
    refactor

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -26,7 +26,7 @@ public class Main {
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
-        
+
     }
 }
 
---
+++ src/test/java/Moudle/ClientEndPoint_JavaxTest.java
@@ -0,0 +1,36 @@
+package Moudle;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+class ClientEndPoint_JavaxTest {
+
+    @Test
+    void testConnect() {
+        Runnable testRun = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ClientEndPoint_Javax clientEndPoint = new ClientEndPoint_Javax(new URI("ws://localhost:5050/websocket"));
+                    clientEndPoint.addMessageHandler(new ClientEndPoint_Javax.MessageHandler() {
+                        public void handleMessage(String message) {
+                            System.out.println(message);
+                        }
+                    });
+                    clientEndPoint.sendMessage("Hello Server");
+                    Thread.sleep(5000);
+                    clientEndPoint.close();
+
+                } catch (URISyntaxException | InterruptedException ex) {
+                    System.err.println("URISyntaxException exception: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+            }
+        };
+        Thread testThread = new Thread(testRun);
+        testThread.setDaemon(true);
+        testThread.start();
+    }
+}
